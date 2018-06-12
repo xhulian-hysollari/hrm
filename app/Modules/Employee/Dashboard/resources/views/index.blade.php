861128d51f26d0d3a197d6f2d98989da4689b70a
hx <xhulian.hysollari@inovap.com>
2018-06-12 18:03:05 +0200

    new features

--- app/Modules/Employee/Dashboard/resources/views/index.blade.php
+++ app/Modules/Employee/Dashboard/resources/views/index.blade.php
@@ -4,6 +4,17 @@
     <div class="row">
         <div class="col-sm-6 col-md-4">
             <div class="card text-white bg-primary text-center">
+                <a class="nav-box" href="{{route('employee.documents.index')}}">
+                    <div class="card-body">
+                        <blockquote class="card-bodyquote">
+                            <h2>Personal Info</h2>
+                        </blockquote>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="card text-white bg-primary text-center">
                 <a class="nav-box" href="{{route('employee.leaves.index')}}">
                     <div class="card-body">
                         <blockquote class="card-bodyquote">
@@ -52,17 +63,6 @@
                     <div class="card-body">
                         <blockquote class="card-bodyquote">
                             <h2>{{trans('app.employee.training.main')}}</h2>
-                        </blockquote>
-                    </div>
-                </a>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="card text-white bg-primary text-center">
-                <a class="nav-box" href="{{route('employee.documents.index')}}">
-                    <div class="card-body">
-                        <blockquote class="card-bodyquote">
-                            <h2>{{trans('app.employee.documents.main')}}</h2>
                         </blockquote>
                     </div>
                 </a>
