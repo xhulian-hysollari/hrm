5e0e0b0f8309188cd0f416bb818efa2beef5c022
hx <xhulian.hysollari@inovap.com>
2018-07-10 16:00:12 +0200

    remove personal info

--- app/Modules/Employee/Dashboard/resources/views/index.blade.php
+++ app/Modules/Employee/Dashboard/resources/views/index.blade.php
@@ -2,17 +2,17 @@
 @section('content')
 
     <div class="row">
-        <div class="col-sm-6 col-md-4">
-            <div class="card text-white bg-primary text-center">
-                <a class="nav-box" href="{{route('employee.documents.index')}}">
-                    <div class="card-body">
-                        <blockquote class="card-bodyquote">
-                            <h2>Personal Info</h2>
-                        </blockquote>
-                    </div>
-                </a>
-            </div>
-        </div>
+        {{--<div class="col-sm-6 col-md-4">--}}
+            {{--<div class="card text-white bg-primary text-center">--}}
+                {{--<a class="nav-box" href="{{route('employee.documents.index')}}">--}}
+                    {{--<div class="card-body">--}}
+                        {{--<blockquote class="card-bodyquote">--}}
+                            {{--<h2>Personal Info</h2>--}}
+                        {{--</blockquote>--}}
+                    {{--</div>--}}
+                {{--</a>--}}
+            {{--</div>--}}
+        {{--</div>--}}
         <div class="col-sm-6 col-md-4">
             <div class="card text-white bg-primary text-center">
                 <a class="nav-box" href="{{route('employee.leaves.index')}}">
